46693556792bd25fa1d64b8bd1c799d3c1050677
Tabrielluu <35918277+Gabriel-Lu@users.noreply.github.com>
2019-07-20 13:34:24 +0800

    通过delete文件把消极、积极和结果分别抽取到三个文件中
    消极因素抽取到negative.csv,积极因素抽取到positive.csv,结果抽取到result.csv
    各个事件由---------为分隔符
    成对的Begin-End间由逗号分开

--- Delete0.py
+++ Delete0.py
@@ -1,7 +1,5 @@
 #-*- coding: utf-8 -*-
-# Function: Delete O type but not number row in csv file
-# Author:Tabrielluunn
-# Date:2019/07/19 Fri
+#function: Delete O type but not number row in csv file
 
 import pandas as pd
 import csv
@@ -30,10 +28,81 @@ OrderFilter= df[df['words'].isin(str500list)]#TESTING
 filter = df[(df['type'].isin(['BN','EN','BP','EP','SP','BR','IR','ER']))|(df['words'].isin(str500list))]
 
 #print(filter.head(50))#TESTING
-filter.to_csv("/home/tabrielluunn/PycharmProjects/untitled/OrderFilter.csv",index=False,header=False)
-#得到了OrderFilter.csv,文件中保留了事件因素，去掉了无意义的一些词
+filter.to_csv("/home/tabrielluunn/PycharmProjects/untitled/OrderFilter.csv",index=False,header=False)#得到了OrderFilter.csv,文件中保留了事件因素，去掉了无意义的一些词
 
+filename='/home/tabrielluunn/PycharmProjects/untitled/OrderFilter.csv'
+#0720 重新写一个CSV文件，合并BN，EN因为无法删除CSV文件的某一行
 
+#把消极事件因素写入到文件negative.csv中
+nFile='/home/tabrielluunn/PycharmProjects/untitled/negative.csv'
+with open(filename) as f:
+    reader=csv.reader(f)
+    for row in reader:
+        #print(row[-1]) #TESTING
+        if(row[-1]!='O'): #如果该行不是标识案例间分割线的1,2,3...
+            if(row[-1] == "BN"):
+                with open(nFile,'a') as nf:
+                    writer=csv.writer(nf)
+                    writer.writerow([row[-2]])
+            if (row[-1] == 'EN'):
+                with open(nFile, 'a') as nf:
+                    writer = csv.writer(nf)
+                    writer.writerow([row[-2]])
+                    writer.writerow(',')
+        else:
+            with open(nFile, 'a') as nf:
+                writer = csv.writer(nf)
+                writer.writerow(['--------------'])
 
+#把积极事件因素写入到文件positive.csv中
+pFile='/home/tabrielluunn/PycharmProjects/untitled/positive.csv'
+with open(filename) as f:
+    reader=csv.reader(f)
+    for row in reader:
+        #print(row[-1]) #TESTING
+        if(row[-1]!='O'): #如果该行不是标识案例间分割线的1,2,3...
+            if(row[-1] == "BP"):
+                with open(pFile,'a') as pf:
+                    writer=csv.writer(pf)
+                    writer.writerow([row[-2]])
+            if (row[-1] == 'EP'):
+                with open(pFile, 'a') as pf:
+                    writer = csv.writer(pf)
+                    writer.writerow([row[-2]])
+                    writer.writerow(',')
+            if (row[-1] == 'SP'):
+                with open(pFile, 'a') as pf:
+                    writer = csv.writer(pf)
+                    writer.writerow([row[-2]])
+        else:
+            with open(pFile, 'a') as pf:
+                writer = csv.writer(pf)
+                writer.writerow(['--------------'])
+
+#把事件结果因素写入到文件result.csv中
+rFile='/home/tabrielluunn/PycharmProjects/untitled/result.csv'
+with open(filename) as f:
+    reader=csv.reader(f)
+    for row in reader:
+        #print(row[-1]) #TESTING
+        if(row[-1]!='O'): #如果该行不是标识案例间分割线的1,2,3...
+            if(row[-1] == "BR"):
+                with open(rFile,'a') as rf:
+                    writer=csv.writer(rf)
+                    writer.writerow([row[-2]])
+            if (row[-1] == 'IR'):
+                with open(rFile, 'a') as rf:
+                    writer = csv.writer(rf)
+                    writer.writerow([row[-2]])
+            if (row[-1] == 'ER'):
+                with open(rFile, 'a') as rf:
+                    writer = csv.writer(rf)
+                    writer.writerow([row[-2]])
+                    writer.writerow(',')
+
+        else:
+            with open(rFile, 'a') as rf:
+                writer = csv.writer(rf)
+                writer.writerow(['--------------'])
 
 
